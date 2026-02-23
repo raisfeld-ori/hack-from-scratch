8067835768fee22c85d0e59b056dee88e79d7779
arielldev <099558er@gmail.com>
2026-02-23 12:19:46 +0200

    im the fucking goat

--- src/intro/intro.py
+++ src/intro/intro.py
@@ -12,3 +12,18 @@ def set_levels(lvls: list[Level]):
 @main_blueprint.route("/")
 def index():
     return render_template("index.html")
+
+    
+@main_blueprint.route("/info")
+def info():
+    return render_template("info.html")
+
+@main_blueprint.route("/return_to_level", methods=["POST"])
+def return_to_level():
+    flag = request.form.get("flag")
+    for level in levels:
+        if level.match_password(flag):
+            response = make_response(redirect(f"/level-{level._level_num}/"))
+            response.set_cookie("current_flag", flag)
+            return response
+    return "Invalid flag!", 400
